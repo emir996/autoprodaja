c75109ba07d16c411dbc4766c2885933f227468a
Novica1989 <moranemora@gmail.com>
2018-12-31 12:05:56 +0100

    functional and main file of site

---
+++ index.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="theme-color" content="#e84e1b">
+<meta name="viewport" content="width=device-width">
+<title>VOLKSWAGEN/Volkswagen</title>
+
+
+
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/jquery.bxslider.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+
+</head>
+
+<body>	
+	<header id="header">
+    
+    	<div id="headerTop">
+        	<div class="wrapper">
+            	<div id="headerTopLeft">
+					<span><i class="fa fa-lg fa-phone"></i>&nbsp;&nbsp;&nbsp;000.000.000.000</span>
+					<span><i class="fa fa-lg fa-envelope-o"></i>&nbsp;&nbsp;&nbsp;volkswagen@gmail.rs</span>
+                </div>
+            	<div id="headerTopRight">
+                	<a href="#" target="_blank"><i class="fa fa-lg fa-facebook-square"></i></a>
+                	<a href="#" target="_blank"><i class="fa fa-lg fa-twitter-square"></i></a>
+                	<a href="#" target="_blank"><i class="fa fa-lg fa-linkedin-square"></i></a>
+                </div>
+            </div>
+        </div>
+        
+        <div id="headerBottom">
+        	<div class="wrapper">
+            
+            	<div id="logo">
+                	<a href="index.php">
+                    	<img src="images/logo.png" alt="logo">
+                    </a>
+                </div>
+                
+                <nav id="nav">
+                	<ul>
+                    	<li><a href="login.php">LOG IN/REGISRACIJA</a></li>
+                    	<li><a href="dileri.php">KORPA</a></li>
+                    	<li><a href="modeli.php">MODELS</a></li>
+                    	
+                    	<li><a href="contact.php">CONTACT</a></li>
+                    </ul>
+                    <a href="#" id="respmenu" class="button"><i class="fa fa-lg fa-navicon"></i>&nbsp;&nbsp;&nbsp;&nbsp;Navigation</a>
+                </nav>
+            
+            </div>
+        </div>   
+    
+    </header><!-- kraj #header -->
+    
+    
+    <article id="hero" class="negative">
+    	<div class="wrapper">
+        
+        
+        	<h1>ARTEON</h1>
+            <p>Sapiente excepturi aspernatur sed cupiditate ducimus</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, possimus, beatae, temporibus provident veritatis iste laboriosam perferendis.</p>
+            <p><a href="modeli.php" class="button">Saznaj vise &raquo;</a></p>
+        
+        </div>    
+    </article>
+    
+    
+    <section id="products">
+    	<div class="wrapper">
+        	
+            <header>
+            	<h2>Our great products</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, esse, fugit, autem, ratione quam doloremque impedit sunt unde repellat ipsum architecto quod itaque ab omnis laudantium modi fugiat accusamus? Explicabo, tempore, est, rerum recusandae at architecto quas cum officiis quaerat accusamus laudantium totam ratione ducimus assumenda omnis laboriosam consectetur velit unde tempora necessitatibus aut harum enim quibusdam sequi dignissimos vitae vero distinctio ipsum minus neque! Mollitia, quae, quod, nam dolorem sequi iste nobis optio ipsam iusto soluta accusantium corporis tenetur et necessitatibus odit officia consequatur accusamus culpa minima nisi atque quibusdam delectus veritatis aliquam tempore sunt quidem maxime quas officiis.</p>
+            </header>
+          
+        </div>    
+    </section>
+    
+    
+    
+    <article id="about" class="negative">
+    	<div class="wrapper">
+        
+        	<div class="back">
+                <h2>About us and our company</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, iure, laboriosam laudantium cumque fuga ab autem nihil quis optio quaerat veniam quo illum ratione accusamus sequi deserunt distinctio enim quidem officiis saepe ullam nesciunt facilis ipsam accusantium nisi velit iste nostrum eveniet ipsa hic? Numquam, aut, quisquam, alias cumque est deleniti eligendi ex animi minima ipsa eos aliquid explicabo officia. Rerum, corrupti deleniti suscipit omnis optio inventore modi iusto tempora quis aliquam aspernatur consectetur hic ducimus aliquid id accusamus labore maiores nobis autem incidunt. Recusandae, doloremque, praesentium, cumque, possimus reprehenderit maxime omnis saepe ipsum vel dolores mollitia porro eligendi corrupti.</p>
+                <p><a href="modeli.php" class="button">Sznaj vise &raquo;</a></p>
+       		</div>
+       
+        </div>
+    </article>
+    
+    
+    <section id="partner">
+    	<div class="wrapper">
+        
+        	<h2>Our famous partners</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, quidem, vitae veritatis distinctio qui amet eligendi ullam accusamus porro dignissimos dolores dolorem quibusdam nemo dolore quasi voluptates modi error laudantium voluptas beatae nulla ipsam repellat quaerat omnis harum expedita voluptatum accusantium voluptatibus soluta deleniti ipsa maxime ipsum delectus. Magnam natus id repellat saepe dignissimos fuga sint doloremque ad rem excepturi! Alias, laborum, officiis, excepturi incidunt amet quos natus odio aliquid ullam quam consectetur est nesciunt voluptatum officia quia! Reiciendis, perferendis, laudantium, corrupti soluta cupiditate impedit fuga voluptates praesentium ratione quae itaque asperiores veniam incidunt adipisci consequuntur! Deserunt, nulla in aspernatur ut suscipit vitae repudiandae laudantium nisi! Obcaecati, corporis, ea, non veniam harum recusandae assumenda repudiandae alias eveniet impedit nulla possimus.</p>
+        
+            
+        </div>
+    </section>
+    
+    
+    
+    <footer id="footer" class="negative">
+    	<div class="wrapper">
+    		
+            <div class="footerBlock">
+            	<h3>Consectetur adipisi</h3>
+            	<p class="bold">Lorem ipsum dolor consectetur adipisicing elit. Doloribus.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, optio, dolore, quibusdam molestiae voluptate cupiditate hic itaque mollitia deserunt maxime assumenda beatae illo delectus earum nisi voluptatibus iure dolor minima culpa enim expedita voluptas quod quis quia iste harum ducimus magni ad ipsam modi.</p>
+            </div>
+            <div class="footerBlock">
+            	<h3>Delectus earum nisi</h3>
+            	<p class="bold">Natus, excepturi, suscipit, voluptates quam veniam repudiandae nemo debitis qui illum</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, optio, dolore, quibusdam molestiae voluptate cupiditate hic itaque mollitia deserunt maxime assumenda beatae illo delectus earum nisi voluptatibus iure dolor minima culpa enim expedita voluptas quod quis quia iste harum ducimus magni ad ipsam modi.</p>
+            </div>
+            
+            <div class="footerBlockR">
+            	<img src="images/logo.png">
+            </div>
+            
+    	</div>
+        <div class="wrapper" id="footerBottom">
+        	<a href="#top" class="button">back to top</a>
+        	<p>Copyright @ Volkswagen// <a href="#">privacy</a> / <a href="#">cookie policy</a> </p>
+        
+        </div>
+    </footer>
+    
+
+</body>
+</html>
